452f77cc6044b9ed0cf86e4a4f238302d421bfc3
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-03-09 16:51:07 +0100

    Create UserRepositoryImpl

--- src/main/java/pl/marcinkielbik/EmployeeManager/repository/implementation/UserRepositoryImpl.java
+++ src/main/java/pl/marcinkielbik/EmployeeManager/repository/implementation/UserRepositoryImpl.java
@@ -2,7 +2,6 @@ package pl.marcinkielbik.EmployeeManager.repository.implementation;
 
 import pl.marcinkielbik.EmployeeManager.models.User;
 import pl.marcinkielbik.EmployeeManager.repository.UserRepository;
-
 import java.util.Collection;
 
 public class UserRepositoryImpl implements UserRepository<User> {
